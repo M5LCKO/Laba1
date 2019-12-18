5eadec9b406fbe5be3b9bcec98d89508687992ea
Денис Стулов <m5lcko.68@gmail.com>
2019-12-19 02:04:57 +0300

    7я без багов

--- Laba 1/MainWindow.xaml.cs
+++ Laba 1/MainWindow.xaml.cs
@@ -55,6 +55,8 @@ namespace Laba_1
                         myAL.Add(number);
                         lbMain.Items.Add(number);
                     }
+                    valueList.Clear();
+                    valueList1.Clear();
                     for (index = 0; index < itemCount; index++)
                     {
                         int num = (int)myAL[index];
@@ -78,12 +80,16 @@ namespace Laba_1
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -123,6 +129,8 @@ namespace Laba_1
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -141,6 +149,8 @@ namespace Laba_1
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -161,6 +171,8 @@ namespace Laba_1
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0") && (tbN.Text != "1"))
             {
@@ -182,6 +194,8 @@ namespace Laba_1
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -204,6 +218,8 @@ namespace Laba_1
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -232,6 +248,8 @@ namespace Laba_1
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {//Стулов Денис 3В 120871
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -257,6 +275,8 @@ namespace Laba_1
         private void Button_Click_11(object sender, RoutedEventArgs e)
         {
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -279,6 +299,8 @@ namespace Laba_1
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -311,6 +333,8 @@ namespace Laba_1
         private void Button_Click_16(object sender, RoutedEventArgs e)
         {
             ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
             Gen(myAL);
             if ((tbN.Text != "") && (tbN.Text != "0"))
             {
@@ -331,11 +355,58 @@ namespace Laba_1
                 {
                     lbMain.Items.Add(elem);
                 }
-                for (int l = 0; l < itemCount - 1; l++)
+                for (int l = 0; l < itemCount; l++)
                 {
                     double num = (double)myAL[l];
                     valueList1.Add(new KeyValuePair<string, double>("", num));
                 }
+
+            }
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            valueList.Clear();
+            valueList1.Clear();
+            Gen(myAL);
+            if ((tbN.Text != "") && (tbN.Text != "0"))
+            {
+                int itemCount = Convert.ToInt32(tbN.Text);
+                double count = 0;
+                double count1 = 0;
+                for (int i = 0; i < itemCount; i++)
+                {
+                    count += (int)myAL[i];
+                }
+                count /= itemCount;
+                lbMain.Items.Add(String.Format("Среднее занчение элементов массива {0}", count));
+                for (int k = 0; k < itemCount; k++)
+                {
+                    count1 += Math.Abs(count - (int)myAL[k]);
+                }
+                count1 /= itemCount;
+                lbMain.Items.Add(String.Format("Среднее занчение всех отклонений от среднего арефметического {0}", count1));
+                for (int k = 0; k < itemCount; k++)
+                {
+                    myAL[k] = Convert.ToDouble(myAL[k]);
+                    if (Math.Abs(count - (double)myAL[k]) > count1 / 2)
+                        myAL[k] = count;
+                }
+                foreach (double elem in myAL)
+                {
+                    lbMain.Items.Add(elem);
+                }
+                valueList1.Clear();
+                foreach (double elem in myAL)
+                {
+                    valueList1.Add(new KeyValuePair<string, double>("", elem));
+                }
+                //for (int l = 0; l < itemCount; l++)
+                //{
+                //    double num = (double)myAL[l];
+                //    valueList1.Add(new KeyValuePair<string, double>("", num));
+                //}
 
             }
         }
