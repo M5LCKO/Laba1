ffeeff542deff721356860bd0e816b687131337b
Денис Стулов <m5lcko.68@gmail.com>
2019-12-19 14:07:18 +0300

    исправлено

--- Laba 1/MainWindow.xaml.cs
+++ Laba 1/MainWindow.xaml.cs
@@ -449,6 +449,8 @@ namespace Laba_1
 
         }
 
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+
         private void Button_Click_19(object sender, RoutedEventArgs e)
         {
             ArrayList myAL = new ArrayList();
@@ -492,6 +494,69 @@ namespace Laba_1
                     valueList1.Add(new KeyValuePair<string, double>("", elem));
                 }
             }
+            }
+        }
+
+        //метод для слияния массивов
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        //сортировка слиянием
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+
+        static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
         }
 
         private void Button_Click_20(object sender, RoutedEventArgs e)
@@ -544,5 +609,6 @@ namespace Laba_1
                 valueList1.Add(new KeyValuePair<string, double>("", elem));
             }
         }
+
     }
 }
